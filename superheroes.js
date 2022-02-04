a3f332efc1d8587f0c697017ed4069fca32294d8
lacj@kea.dk <lacj@kea.dk>
2022-02-04 08:34:24 +0100

    start

---
+++ superheroes.js
@@ -0,0 +1,29 @@
+const superheroes = [
+  {
+    alias: "Dr. Mc Perfection",
+    birthname: "None of your business",
+    dob: "9999-09-09",
+    cape: false,
+    powers: ["SmartAss", "NightVision", "RentgenVision", "WalkingLieDetector"],
+    img: null,
+    gender: "N/A",
+    species: "Doc",
+  },
+  {
+    alias: "Invisible Man",
+    birthname: "Obvious True",
+    dob: "1976-05-24",
+    cape: false,
+    powers: ["Invisibility", "silence"],
+    img: "https://ats-heritage.co.uk/wp-content/uploads/2016/09/iStock_96204197_XLARGE.jpg",
+    gender: "man",
+    species: "inhuman",
+  },
+];
+
+alert(
+  "1. Checking if JS file is linked up correctly. \n2. Always open the inspector/console when woorking with the JS \n3. Write your code in the end of the js-file \n4. Don't use alert boxes for UI... \n5. Delete this message or comment it out"
+);
+console.table(superheroes);
+
+/* write your code here */
